a9891318c22bf551230c871d145f2cf5a9261642
kingdomstay <kirill.troitskij@gmail.com>
2022-01-19 10:40:55 +0300

    Добавлена страница с авторизацией и выходом из системы, видоизменён базовый шаблон

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('upload/', views.upload, name='upload'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from mainapp.forms import LoginForm
+
+
 def index(request):
     """
     Функция отображения для домашней страницы сайта.
@@ -11,3 +16,48 @@ def index(request):
         request,
         'index.html'
     )
+
+
+@login_required
+def upload(request):
+    return 'test'
+
+
+# def user_register(request):
+#     if request.method == 'POST':
+#         user_form = RegisterForm(request.POST)
+#         if user_form.is_valid():
+#             # Create a new user object but avoid saving it yet
+#             new_user = user_form.save(commit=False)
+#             # Set the chosen password
+#             new_user.set_password(user_form.cleaned_data['password'])
+#             # Save the User object
+#             new_user.save()
+#             return render(request, 'account/register_done.html', {'new_user': new_user})
+#     else:
+#         user_form = UserRegistrationForm()
+#     return render(request, 'account/register.html', {'user_form': user_form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('index')
