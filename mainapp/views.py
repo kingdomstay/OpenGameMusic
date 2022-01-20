d0f26ed3b50a07f7db4ac963937dfc58101236bd
kingdomstay <kirill.troitskij@gmail.com>
2022-01-20 14:26:22 +0300

    Перенесены исходные стили .scss и внесены небольшие исправления в исходные

--- mainapp/views.py
+++ mainapp/views.py
@@ -24,22 +24,6 @@ def upload(request):
     return 'test'
 
 
-# def user_register(request):
-#     if request.method == 'POST':
-#         user_form = RegisterForm(request.POST)
-#         if user_form.is_valid():
-#             # Create a new user object but avoid saving it yet
-#             new_user = user_form.save(commit=False)
-#             # Set the chosen password
-#             new_user.set_password(user_form.cleaned_data['password'])
-#             # Save the User object
-#             new_user.save()
-#             return render(request, 'account/register_done.html', {'new_user': new_user})
-#     else:
-#         user_form = UserRegistrationForm()
-#     return render(request, 'account/register.html', {'user_form': user_form})
-
-
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
@@ -51,9 +35,11 @@ def user_login(request):
                     login(request, user)
                     return redirect('index')
                 else:
-                    return HttpResponse('Disabled account')
+                    form = LoginForm()
+                    return render(request, 'login.html', {'message': 'Ваш аккаунт был деактивирован.', 'form': form})
             else:
-                return HttpResponse('Invalid login')
+                form = LoginForm()
+                return render(request, 'login.html', {'message': 'Введён неверный логин или пароль.', 'form': form})
     else:
         form = LoginForm()
     return render(request, 'login.html', {'form': form})
